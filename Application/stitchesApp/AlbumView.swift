008fe654144d6fc6e2b1f00cf0b60388a29da2bd
Laurie Tweed <laurie.tweed@gmail.com>
2025-01-25 03:54:21 -0500

    Adding yarn inventory and project list views to full content view, other view adjustments.

---
+++ Application/stitchesApp/AlbumView.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosView.swift
+//  stitchesApp
+//
+//  Created by Laurie on 1/25/25.
+//
+
+import SwiftUI
+import CoreData
+
+struct PhotosView: View {
+    var body: some View {
+        Text("PhotosView")
+    }
+}
